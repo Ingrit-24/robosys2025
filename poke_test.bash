19b3420c0cbd5fe79689243a40c15972574b8e76
gocchi <shogo@gocchi.localdomain>
2025-12-18 13:21:56 +0900

    テスト項目追加

--- poke_test.bash
+++ poke_test.bash
@@ -8,13 +8,37 @@ ng(){
 }
 res=0
 
-readarray -t arr < <(echo garchomp | ./poke )
 expected=(108 130 95 80 85 102)
 
+#すべて小文字
+readarray -t arr_1 < <(echo garchomp | ./poke)
 for i in $(seq 0 5); do
-        [ "${arr[$i]}" == "${expected[$i]}" ] || ng "$LINENO"
+        [ "${arr_1[$i]}" == "${expected[$i]}" ] || ng "$LINENO"
 done
 
+#すべて大文字
+readarray -t arr_2 < <(echo GARCHOMP | ./poke)
+for i in $(seq 0 5); do
+        [ "${arr_2[$i]}" == "${expected[$i]}" ] || ng "$LINENO"
+done
+
+#一文字目のみ大文字
+readarray -t arr_3 < <(echo Garchomp | ./poke)
+for i in $(seq 0 5); do
+        [ "${arr_3[$i]}" == "${expected[$i]}" ] || ng "$LINENO"
+done
+
+#大文字ランダム
+readarray -t arr_4 < <(echo GarChoMp | ./poke)
+for i in $(seq 0 5); do
+        [ "${arr_4[$i]}" == "${expected[$i]}" ] || ng "$LINENO"
+done
+
+#空白の多々しい処理確認
+readarray -t arr_5 < <(echo garchomp                   | ./poke)
+for i in $(seq 0 5); do
+        [ "${arr_5[$i]}" == "${expected[$i]}" ] || ng "$LINENO"
+done
 
 out=$(echo hoge | ./poke)
 status=$?
